d580d20898810e397a5c6ab7e96043a0ec5154df
Kent Fredric <kentfredric@gmail.com>
2017-04-22 02:01:15 +1200

    Test against pgjdbc

--- test/irresponsible/utrecht_test.clj
+++ test/irresponsible/utrecht_test.clj
@@ -20,11 +20,14 @@
            [clojure.lang ExceptionInfo]))
 
 (def defaults
-  {:postgres
-  {:adapter "postgresql"
-   :server-name "127.0.0.1"
-   :port-number 5432
-   :connection-timeout 5000}})
+  {:postgres  {:adapter "postgresql"
+               :server-name "127.0.0.1"
+               :port-number 5432
+               :connection-timeout 5000}
+   :pgjdbc-ng {:adapter "pgjdbc-ng"
+               :host "127.0.0.1"
+               :port 5432
+               :connection-timeout 5000}})
 
 (def config
   (let [uth  (env :utrecht-test-host)
@@ -34,63 +37,80 @@
         utp  (env :utrecht-test-pass)
         utp2 (env :utrecht-test-port)]
     {:postgres
-    (cond-> (:postgres defaults)
-      uth  (assoc :server-name uth)
-      utct (assoc :connection-timeout utct)
-      utd  (assoc :database-name utd)
-      utu  (assoc :username utu)
-      utp  (assoc :password utp)
-      utp2 (assoc :port-number (Integer/parseInt utp2)))}))
+     (cond-> (:postgres defaults)
+       uth  (assoc :server-name uth)
+       utct (assoc :connection-timeout utct)
+       utd  (assoc :database-name utd)
+       utu  (assoc :username utu)
+       utp  (assoc :password utp)
+       utp2 (assoc :port-number (Integer/parseInt utp2)))
+     :pgjdbc-ng
+     (cond-> (:pgjdbc-ng defaults)
+       uth  (assoc :host uth)
+       utct (assoc :network-timeout utct)
+       utd  (assoc :database utd)
+       utu  (assoc :username utu)
+       utp  (assoc :password utp)
+       utp2 (assoc :port (Integer/parseInt utp2)))}))
 
-(if (not (contains? (:postgres config) :database-name))
-  (println "* Tests Skipped, UTRECHT_TEST_DB must be configured to a test Posgresql database")
-  (do
 
-    (apply printf "* Testing vs %s:%s/%s\n" (map (:postgres config) [:server-name :port-number :database-name]))
-    (deftest utrecht
-      (let [pool (hikaricp (:postgres config))]
-        (is (satisfies? u/Pool pool))
-        (u/with-conn [c pool]
-          (is (satisfies? u/Conn c))
-          (is (satisfies? u/Conn c))
-          (u/with-prep c [p "select 'foo' as result"]
-            (is (instance? PreparedStatement p))
-            (is (= '({:result "foo"})
-                   (u/query c p)
-                   (u/query c "select 'foo' as result")))
-            (is (= [0] (u/execute c "create temporary table foo()")))))
-        (try (u/with-transaction :ro :serializable [c pool]
-            (let [sp1 (u/savepoint c :sp1)]
-               (is (instance? Savepoint sp1))
-               (is (= '({:result "foo"})
-                 (u/query c "select 'foo' as result")))
-               (is (nil? (u/rollback c sp1))))
-               (throw (ex-info "" {:error :yup})))
-             (catch ExceptionInfo e
-               (is (= {:error :yup} (ex-data e)))))
-        (.close pool)))
+(when-not (contains? (:postgres config) :database-name)
+  (println "* Tests Skipped, UTRECHT_TEST_DB must be configured to a test Posgresql database"))
 
-    (deftest cpt
-      (let [pool (start (c/utrecht (:postgres config)))]
-        (is (satisfies? u/Pool pool))
-        (u/with-conn [c pool]
-          (is (satisfies? u/Conn c))
-          (is (satisfies? u/Conn c))
-          (u/with-prep c [p "select 'foo' as result"]
-            (is (instance? PreparedStatement p))
-            (is (= '({:result "foo"})
-                   (u/query c p)
-                   (u/query c "select 'foo' as result")))
-            (is (= [0] (u/execute c "create temporary table foo()")))))
-        (try (u/with-transaction :ro :serializable [c pool]
-            (let [sp1 (u/savepoint c :sp1)]
-               (is (instance? Savepoint sp1))
-               (is (= '({:result "foo"})
-                 (u/query c "select 'foo' as result")))
-               (is (nil? (u/rollback c sp1))))
-               (throw (ex-info "" {:error :yup})))
-             (catch ExceptionInfo e
-               (is (= {:error :yup} (ex-data e)))))
-        (stop pool)))
+(when-not (contains? (:pgjdbc-ng config) :username)
+  (println "* pgjdbc-tests will fail, doesn't support auto-username, please set UTRECHT_TEST_USER"))
 
-))
+(deftest utrecht
+  (if (contains? (:postgres config) :database-name)
+    (doseq [k [:postgres :pgjdbc-ng]]
+      (testing k
+        (let [pool (hikaricp (config k))]
+          (is (satisfies? u/Pool pool))
+          (u/with-conn [c pool]
+            (is (satisfies? u/Conn c))
+            (is (satisfies? u/Conn c))
+            (u/with-prep c [p "select 'foo' as result"]
+              (is (instance? PreparedStatement p))
+              (is (= '({:result "foo"})
+                     (u/query c p)
+                     (u/query c "select 'foo' as result")))
+              (is (= [0] (u/execute c "create temporary table foo()")))))
+          (try (u/with-transaction :ro :serializable [c pool]
+                 (let [sp1 (u/savepoint c :sp1)]
+                   (is (instance? Savepoint sp1))
+                   (is (= '({:result "foo"})
+                          (u/query c "select 'foo' as result")))
+                   (is (nil? (u/rollback c sp1))))
+                 (throw (ex-info "" {:error :yup})))
+               (catch ExceptionInfo e
+                 (is (= {:error :yup} (ex-data e)))))
+          (.close pool))))
+      (println "* Tests Skipped, UTRECHT_TEST_DB must be configured to a test Posgresql database")))
+
+(deftest cpt
+  (if (contains? (:postgres config) :database-name)
+    (doseq [k [:postgres :pgjdbc-ng]]
+      (testing k
+        (let [pool (start (c/utrecht (config k)))]
+          (is (satisfies? u/Pool pool))
+          (u/with-conn [c pool]
+            (is (satisfies? u/Conn c))
+            (is (satisfies? u/Conn c))
+            (u/with-prep c [p "select 'foo' as result"]
+              (is (instance? PreparedStatement p))
+              (is (= '({:result "foo"})
+                     (u/query c p)
+                     (u/query c "select 'foo' as result")))
+              (is (= [0] (u/execute c "create temporary table foo()")))))
+          (try (u/with-transaction :ro :serializable [c pool]
+                 (let [sp1 (u/savepoint c :sp1)]
+                   (is (instance? Savepoint sp1))
+                   (is (= '({:result "foo"})
+                          (u/query c "select 'foo' as result")))
+                   (is (nil? (u/rollback c sp1))))
+                 (throw (ex-info "" {:error :yup})))
+               (catch ExceptionInfo e
+                 (is (= {:error :yup} (ex-data e)))))
+          (stop pool))))
+      (println "* Tests Skipped, UTRECHT_TEST_DB must be configured to a test Posgresql database")))
+
